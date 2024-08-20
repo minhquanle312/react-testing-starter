214292dcc5e9330a9ec10b9586a79eaa50191d0f
Minh Quân Lê <minhquanle@gmail.com>
2024-08-21 00:10:19 +0700

    update test

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -3,6 +3,7 @@ import ProductDetail from '../../src/components/ProductDetail'
 import { server } from '../mocks/server'
 import { http, HttpResponse } from 'msw'
 import { db } from '../mocks/db'
+import AllProviders from '../AllProviders'
 
 describe('ProductDetail', () => {
   let productId: number
@@ -20,7 +21,7 @@ describe('ProductDetail', () => {
     const product = db.product.findFirst({
       where: { id: { equals: productId } },
     })
-    render(<ProductDetail productId={productId} />)
+    render(<ProductDetail productId={productId} />, { wrapper: AllProviders })
 
     expect(
       await screen.findByText(new RegExp(product!.name))
@@ -33,13 +34,13 @@ describe('ProductDetail', () => {
   it('should render message if product not found', async () => {
     server.use(http.get('/products/1', () => HttpResponse.json(null)))
 
-    render(<ProductDetail productId={1} />)
+    render(<ProductDetail productId={1} />, { wrapper: AllProviders })
 
     expect(await screen.findByText(/not found/i)).toBeInTheDocument()
   })
 
   it('should render an error for invalid productId', async () => {
-    render(<ProductDetail productId={0} />)
+    render(<ProductDetail productId={0} />, { wrapper: AllProviders })
 
     expect(await screen.findByText(/invalid/i)).toBeInTheDocument()
   })
@@ -47,7 +48,7 @@ describe('ProductDetail', () => {
   it('should render an error message when there is an error', async () => {
     server.use(http.get('/products/1', () => HttpResponse.error()))
 
-    render(<ProductDetail productId={1} />)
+    render(<ProductDetail productId={1} />, { wrapper: AllProviders })
 
     expect(await screen.findByText(/error/i)).toBeInTheDocument()
   })
--- tests/components/ProductList.test.tsx
+++ tests/components/ProductList.test.tsx
@@ -3,10 +3,11 @@ import {
   screen,
   waitForElementToBeRemoved,
 } from '@testing-library/react'
+import { delay, http, HttpResponse } from 'msw'
 import ProductList from '../../src/components/ProductList'
-import { server } from '../mocks/server'
-import { http, HttpResponse, delay } from 'msw'
+import AllProviders from '../AllProviders'
 import { db } from '../mocks/db'
+import { server } from '../mocks/server'
 
 describe('ProductList', () => {
   const productIds: number[] = []
@@ -24,7 +25,7 @@ describe('ProductList', () => {
   })
 
   it('should render the list of products', async () => {
-    render(<ProductList />)
+    render(<ProductList />, { wrapper: AllProviders })
 
     const items = await screen.findAllByRole('listitem')
     expect(items.length).toBeGreaterThan(0)
@@ -33,7 +34,7 @@ describe('ProductList', () => {
   it('should render no products available if no product is found', async () => {
     server.use(http.get('/products', () => HttpResponse.json([])))
 
-    render(<ProductList />)
+    render(<ProductList />, { wrapper: AllProviders })
 
     const message = await screen.findByText(/no products/i)
     expect(message).toBeInTheDocument()
@@ -42,7 +43,7 @@ describe('ProductList', () => {
   it('should render an error message when there is an error', async () => {
     server.use(http.get('/products', () => HttpResponse.error()))
 
-    render(<ProductList />)
+    render(<ProductList />, { wrapper: AllProviders })
 
     expect(await screen.findByText(/error/i)).toBeInTheDocument()
   })
@@ -55,15 +56,13 @@ describe('ProductList', () => {
       })
     )
 
-    render(<ProductList />)
+    render(<ProductList />, { wrapper: AllProviders })
 
     expect(await screen.findByText(/loading/i)).toBeInTheDocument()
   })
 
   it('should remove the loading indicator after data is fetched', async () => {
-    // server.use(http.get('/products', () => HttpResponse.error()))
-
-    render(<ProductList />)
+    render(<ProductList />, { wrapper: AllProviders })
 
     await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
   })
